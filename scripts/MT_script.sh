02c1dea0fdfe2190b456941ea95774b5dc7c13d9
Khalid Almubarak <49135754+KhalidAlt@users.noreply.github.com>
2023-02-22 18:36:04 +0300

    Create MT_script.sh

---
+++ scripts/MT_script.sh
@@ -0,0 +1,15 @@
+model_id="facebook/m2m100-12B-last-ckpt"
+data_name='conceptual_captions'
+cache_dir="/home/ubuntu/ccg/cache/"
+save_path="/home/ubuntu/ccg/dataset/"
+
+
+python translate.py  \
+    --model_name_or_path $model_id \
+    --tokenizer_name_or_path $model_id \
+    --dataset $data_name \
+    --split "train" \
+    --subset 'unlabeled' \
+    --column_name "caption" \
+    --device "cuda" \
+    --save_path $save_path \
